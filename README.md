c1e804230a68b913523924d03082e1870c9b2cc7
DBProductions <dennis@dbproductions.de>
2015-01-29 23:08:34 +0100

    make some small changes and adjust README

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-xaudioplayer
-============
+# xaudioplayer
 
-Player for HTML5 Audio
+Object to use HTML5 Audio with different channels.  
+Initialized with an array of sounds to play them.
+
+## Feedback
+Star this repo if you found it useful. Use the github issue tracker to give feedback on this repo.
--- xaudioplayer.js
+++ xaudioplayer.js
@@ -1,29 +1,32 @@
-function XAudioPlayer(path, sounds) {
+function XAudioPlayer(path, sounds, controls) {
     "use strict";
     this.path = path || '';
     this.channelSrc = sounds || [];
     this.channelSrcLength = this.channelSrc.length;    
     // create audio tag and set attributes
-    for (var i = 0; i < this.channelSrcLength; i++) {		
+    for (var i = 0; i < this.channelSrcLength; i++) {       
         var audioTag = document.createElement('audio');
         audioTag.setAttribute("id", sounds[i]);
         audioTag.setAttribute("preload", "auto");
         audioTag.setAttribute("autobuffer", "true");
+        if (controls) {
+            audioTag.setAttribute("controls", "true");    
+        }                
         if (!!audioTag.canPlayType && "" !== audioTag.canPlayType('audio/ogg; codecs="vorbis"')) {
             var sourceTagOgg = document.createElement('source');
             sourceTagOgg.setAttribute("src", this.path + sounds[i] + '.ogg');
             audioTag.appendChild(sourceTagOgg);
-		}		
-		if (!!audioTag.canPlayType && "" !== audioTag.canPlayType('audio/mpeg')) {		
+        }       
+        if (!!audioTag.canPlayType && "" !== audioTag.canPlayType('audio/mpeg')) {      
             var sourceTagMp3 = document.createElement('source');
             sourceTagMp3.setAttribute("src", this.path + sounds[i] + '.mp3');
             audioTag.appendChild(sourceTagMp3);
-		}		
+        }       
         // IE need it like this
         var embedTag = document.createElement('embed');
         embedTag.setAttribute("src", this.path + sounds[i] + '.ogg');
         audioTag.appendChild(embedTag);
-        document.body.appendChild(audioTag);						
+        document.body.appendChild(audioTag);                        
     }    
     this.audiochannels = [];
     for (var a = 0; a < this.channelSrcLength; a++) {
